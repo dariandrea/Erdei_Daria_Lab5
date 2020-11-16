41f3a6a82c205f112685c64e62f7792cf5aea028
Daria Erdei <dariandrea@yahoo.com>
2020-11-16 19:54:02 +0200

    Add project files.

---
+++ MyPhoneNumbersTableAdapter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+namespace Erdei_Daria_Lab5.PhoneNumbersDataSetTableAdapters
+{
+    public partial class PhoneNumbersTableAdapter
+    {
+        public PhoneNumbersTableAdapter(String connectionString)
+        {
+            this.ClearBeforeFill = true;
+            this.Connection = new
+           System.Data.SqlClient.SqlConnection(connectionString);
+        }
+    }
+}
